c1f641c3a3064f3a011be591865695dcf33adb28
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-12-14 16:07:56 +0100

    no advanced mode

--- src/components/Builder/ProteinBuilder/MartinizeForm.tsx
+++ src/components/Builder/ProteinBuilder/MartinizeForm.tsx
@@ -258,46 +258,6 @@ export default function MartinizeForm(props: MartinizeFormProps) {
             />
           </Grid>
         </React.Fragment>}
-
-        <Grid item xs={12} sm={12} className={classes.formContainer}>
-          <FormControlLabel
-              control={<Checkbox 
-                value={props.advanced}
-                checked={props.advanced === 'true'} 
-                onChange={props.advancedActivate} 
-              />}
-              label="Activate advanced mode"
-            />
-        </Grid>
-
-              
-          <Grid item xs={12} sm={10} className={classes.formContainer}>
-            <TextField 
-              variant="outlined"
-              className={classes.textField}
-              label="Command Line"
-              type="text"
-              disabled={props.advanced === 'false'}
-              value={props.commandline}
-              onChange={e => {props.onAdvancedChange(e.target.value)}}
-            />
-          </Grid>
-          <Grid item xs={12} sm={2} className={classes.formContainer}>
-            <IconButton aria-label="copy" onClick={() => {
-              navigator.clipboard.writeText(props.commandline);
-              setOpenCopied(true);
-            }}>
-              <SvgIcon>
-                <path d="M4 7V21H18V23H4C2.9 23 2 22.1 2 21V7H4M20 3C21.1 3 22 3.9 22 5V17C22 18.1 21.1 19 20 19H8C6.9 19 6 18.1 6 17V5C6 3.9 6.9 3 8 3H11.18C11.6 1.84 12.7 1 14 1C15.3 1 16.4 1.84 16.82 3H20M14 3C13.45 3 13 3.45 13 4C13 4.55 13.45 5 14 5C14.55 5 15 4.55 15 4C15 3.45 14.55 3 14 3M10 7V5H8V17H20V5H18V7H10Z" />
-              </SvgIcon>
-            </IconButton>
-          </Grid>
-
-          <Snackbar anchorOrigin={{vertical: "bottom", horizontal: "left"}} open={openCopied} autoHideDuration={2000} onClose={() => setOpenCopied(false)}>
-            <Alert variant="filled" severity="success">
-              Command line copied to clipboard
-            </Alert>
-          </Snackbar>
         
           <Grid item xs={12} sm={12} className={classes.formContainer}>
           <FormControlLabel
