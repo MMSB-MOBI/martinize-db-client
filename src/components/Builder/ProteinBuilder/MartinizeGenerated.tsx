bbb05cd69601fd9b8ce71ffdb1aac063f09d7a18
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-12-14 16:05:38 +0100

    keep warnings when apply bonds

--- src/components/Builder/ProteinBuilder/MartinizeGenerated.tsx
+++ src/components/Builder/ProteinBuilder/MartinizeGenerated.tsx
@@ -39,7 +39,7 @@ interface MartinizeGeneratedProps {
 
   onGoEditorStart(): any;
 
-  martinizeWarnings?: any;
+  martinizeWarnings: any;
 
   beadRadiusFactor : number; 
   onBeadRadiusChange(_: any, value : number|number[]): any
